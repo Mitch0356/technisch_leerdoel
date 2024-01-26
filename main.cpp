0b2ea91b87f25bb93f76a0f8d93cc47e96cfb9a7
Mitchel van de Pest <mc.vandepest@student.han.nl>
2024-01-26 12:10:36 +0100

    tussentijds

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
+
+#include <dlfcn.h>
+#include <iostream>
 #include "simple_class.hpp"
 
-int main()
+using namespace std;
+
+int main(int argc, char **argv)
 {
-    SimpleClass gerrit = SimpleClass();
-    gerrit.set_name("gerrit");
-    gerrit.add_to_vector(1);
-    return 1;
+  /* on Linux, use "./myclass.so" */
+  void* handle = dlopen("myclass.so", RTLD_LAZY);
+
+  SimpleClass* (*create)();
+  void (*destroy)(SimpleClass*);
+
+  create = (SimpleClass* (*)())dlsym(handle, "create_object");
+  destroy = (void (*)(SimpleClass*))dlsym(handle, "destroy_object");
+
+  SimpleClass* myClass = (SimpleClass*)create();
+  myClass->set_name("gerrit");
+  destroy( myClass );
 }
--- readme.md
+++ readme.md
@@ -4,6 +4,12 @@
 g++ -shared -o simple_class.so -fPIC simple_class.cpp
 ```
 
+## Compileren van het programma dat gebruik maakt van de SO:
+```bash
+g++ -o main main.cpp -lsimple_class -L ./simple_class.so
+```
+
+
 ## Installeren van Ghidra
 Bij deze installatie is gebruik gemaakt van Ubuntu 22.04 ARM64.
 Installeren van Ghidra:
@@ -13,6 +19,6 @@ sudo snap install ghidra
 Dit duurt even.
 
 ## Gebruiken van Ghidra
-
+[gebruikte tutorial/naslag werk](https://www.retroreversing.com/intro-decompiling-with-ghidra)
 
 
